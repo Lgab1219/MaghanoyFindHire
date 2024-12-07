c28018a6f0cf6ae02882c519ec3fe135a34d8c9d
lgab1219 <lancegabrielmaghanoy@gmail.com>
2024-12-07 16:15:41 +0800

    added css to login and register

--- login.php
+++ login.php
@@ -12,14 +12,17 @@ require_once 'core/models.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FindHire Login</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">    <title>FindHire Login</title>
 </head>
 <body>
 
     
     <div class="nav_bar">
-        <span class="logo">
-            <h1>FindHire</h1>
+        <span class="logo_container">
+            <img src="resources/FindHire_Logo.png" alt="findhire_logo" id="logo">
         </span>
     </div>
     
@@ -30,33 +33,32 @@ require_once 'core/models.php';
             <div><?php echo $_SESSION['message']; ?></div>
         <?php unset($_SESSION['message']); }  ?>
 
-        <div class="form">
-            <h2>Login</h2>
-            <form action="core/handleForms.php" method="POST">
-                <label for="email">Email</label>
-                <input type="text" name="email" id="login_email" required>
+        <div class="login_form_container">
+            <h2 id="login-text">Login</h2>
+            <form action="core/handleForms.php" method="POST" id="form">
+                <label for="email" class="login_email">Email</label><br>
+                <input type="text" name="email" class="login_email" style="font-size: 1.5em; border: 1px solid #FF6700; border-radius: 5px;" required>
                 <br><br>
 
-                <label for="password">Password</label>
-                <input type="password" name="password" id="login_password" required>
+                <label for="password" class="login_password">Password</label><br>
+                <input type="password" name="password" class="login_password" style="font-size: 1.5em; border: 1px solid #FF6700; border-radius: 5px;" required>
                 <br><br>
 
-                <input type="submit" value="Login" name="loginUserBtn">
+                <input type="submit" value="Login" name="loginUserBtn" id="login_button">
             </form>
-            <p>Don't have an account? Register <a href="register.php">here</a>!</p>
+            <p style="margin-top: 3em;">Don't have an account? Register <a href="register.php">here</a>!</p>
         </div>
 
-        <div class="form_content">
+        <div class="log_form_content">
             <span>
-                <h1>Your Next Career,<br> Simplified.</h1>
-                <h2>Lightning-Fast Performance: </h2><p>Experience blazing-fast load times and seamless navigation.</p>
-                <h2>Intuitive Interface: </h2><p>A user-friendly design that makes your job search a breeze.</p>
-                <h2>Secure and Private: </h2><p>Protect your personal information with our robust security measures.</p>
+                <h1 style="color: #004E98;">Your Next Career,<br><h1 style="color: #FF6700;">Simplified.</h1></h1><br>
+                <h2 style="color: #004E98;">Lightning-Fast Performance: </h2><p >Experience blazing-fast load times and seamless navigation.</p>
+                <h2 style="color: #004E98;">Intuitive Interface: </h2><p>A user-friendly design that makes your job search a breeze.</p>
+                <h2 style="color: #004E98;">Secure and Private: </h2><p>Protect your personal information with our robust security measures.</p>
             </span>
         </div>
 
     <br><br>
-    <button><a href="core/unset.php" style="text-decoration: none; color: black;">Reset</a></button>
 
     </div>
 </body>
--- register.php
+++ register.php
@@ -12,13 +12,17 @@ require_once 'core/handleForms.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">    <title>FindHire Login</title>
     <title>FindHire Register</title>
 </head>
 <body>
 
     <div class="nav_bar">
-        <span class="logo">
-            <h1>FindHire</h1>
+        <span class="logo_container">
+            <img src="resources/FindHire_Logo.png" alt="findhire_logo" id="logo">
         </span>
     </div>
 
@@ -30,43 +34,43 @@ require_once 'core/handleForms.php';
             <div class="message"><?php echo $_SESSION['message']; ?></div>
         <?php unset($_SESSION['message']); } ?>
 
-        <div class="form_content">
+        <div class="reg_form_content">
             <span>
-                <h1>Your Next Career,<br> Simplified.</h1>
-                <h2>Lightning-Fast Performance: </h2><p>Experience blazing-fast load times and seamless navigation.</p>
-                <h2>Intuitive Interface: </h2><p>A user-friendly design that makes your job search a breeze.</p>
-                <h2>Secure and Private: </h2><p>Protect your personal information with our robust security measures.</p>
+                <h1 style="color: #004E98;">Your Next Career,<br><h1 style="color: #FF6700;">Simplified.</h1></h1><br>
+                <h2 style="color: #004E98;">Lightning-Fast Performance: </h2><p >Experience blazing-fast load times and seamless navigation.</p>
+                <h2 style="color: #004E98;">Intuitive Interface: </h2><p>A user-friendly design that makes your job search a breeze.</p>
+                <h2 style="color: #004E98;">Secure and Private: </h2><p>Protect your personal information with our robust security measures.</p>
             </span>
         </div>
 
-        <div class="form">
-            <h2>Register</h2>
+        <div class="register_form_container">
+            <h2 id="register-text">Register</h2>
             <form action="core/handleForms.php" method="POST">
-                <label for="fname">First Name</label>
-                <input type="text" name="fname" id="register_fname">
+                <label for="fname" class="register_fname">First Name</label>
+                <input type="text" name="fname" class="register_fname" style="border: 1px solid #003B75; border-radius: 5px;">
                 <br><br>
 
-                <label for="lname">Last Name</label>
-                <input type="text" name="lname" id="register_lname">
+                <label for="lname" class="register_lname">Last Name</label>
+                <input type="text" name="lname" class="register_lname" style="border: 1px solid #003B75; border-radius: 5px;">
                 <br><br>
 
-                <label for="email">Email</label>
-                <input type="text" name="email" id="register_email">
+                <label for="email" class="register_email">Email</label>
+                <input type="text" name="email" class="register_email" style="border: 1px solid #003B75; border-radius: 5px;">
                 <br><br>
 
-                <label for="password">Password</label>
-                <input type="password" name="password" id="register_password">
+                <label for="password" class="register_password">Password</label>
+                <input type="password" name="password" class="register_password" style="border: 1px solid #003B75; border-radius: 5px;">
                 <br><br>
 
-                <label for="role">Role</label>
-                <select name="role" id="register_role">
-                    <option value="none" selected disabled></option>
-                    <option value="hr">Human Resources (HR)</option>
-                    <option value="applicant">Applicant</option>
+                <label for="role" class="register_role">Role</label>
+                <select name="role" class="register_role" style="border: 1px solid #003B75; border-radius: 5px;">
+                    <option value="none" selected disabled style="border: 1px solid #003B75; border-radius: 5px;"></option>
+                    <option value="hr" style="border: 1px solid #003B75; border-radius: 5px;">Human Resources (HR)</option>
+                    <option value="applicant" style="border: 1px solid #003B75; border-radius: 5px;">Applicant</option>
                 </select>
                 <br><br>
 
-                <input type="submit" value="Register" name="registerUserBtn">
+                <input type="submit" value="Register" name="registerUserBtn" id="register_button">
             </form>
             <p>Already have an account? Log in <a href="login.php">here</a>!</p>
         </div>
